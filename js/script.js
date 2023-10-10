b2519a438c4a5ddc5b621d8638cb66f988942b84
alexander <aldml0201@gmail.com>
2023-10-10 15:35:42 -0400

    modo oscuro

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 // Add some interactivity to the website
 console.log("Hello, world!");
+
+const swith = document.querySelector(".switch");
+
+swith.addEventListener("click", e => {
+    swith.classList.toggle("active");
+    document.body.classList.toggle("active")
+})
